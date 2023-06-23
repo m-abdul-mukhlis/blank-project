3c732796f91b88f4979f0cdbdece96a96bf2a8c3
mukhlis <mukhlis@fisip.net>
2023-06-23 14:02:03 +0700

    init

---
+++ modules/user/login.tsx
@@ -0,0 +1,18 @@
+// withHooks
+
+import React from 'react';
+import { View } from 'react-native';
+
+
+export interface UserLoginArgs {
+
+}
+export interface UserLoginProps {
+
+}
+export default function m(props: UserLoginProps): any {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'orange' }}>
+    </View>
+  )
+}
